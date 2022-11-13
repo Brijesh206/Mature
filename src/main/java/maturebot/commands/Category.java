37a9a2d858235b1ba14a10437bf3b3848768a9b3
Brijesh <brijeshbhesaniya12@gmail.com>
2022-11-13 19:13:11 +0530

    Add category class to define the categories of the command

---
+++ src/main/java/maturebot/commands/Category.java
@@ -0,0 +1,31 @@
+package maturebot.commands;
+
+/**
+ * Category that represents a group of similar commands.
+ * Each category has a name and an emoji.
+ *
+ * @author Brijesh
+ */
+
+public enum Category {
+    STAFF(":computer:", "Staff"),
+    LEVELS(":chart_with_upwards_trend:", "Levels"),
+    MUSIC(":musical_note:", "Music"),
+    ECONOMY(":moneybag:", "Economy"),
+    STARBOARD(":star:", "Starboard"),
+    FUN(":smile:", "Fun"),
+    AUTOMATION(":gear:", "Automation"),
+    UTILITY(":tools:", "Utility"),
+    GREETINGS(":wave:", "Greetings"),
+    SUGGESTIONS(":thought_balloon:", "Suggestions"),
+    CASINO(":game_die:", "Casino"),
+    PETS(":dog:", "Pets");
+
+    public final String emoji;
+    public final String name;
+
+    Category(String emoji, String name) {
+        this.emoji = emoji;
+        this.name = name;
+    }
+}
